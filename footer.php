2c65b4f0c00ab6ef26523f09e7447d35e410bd9d
minhtu250695 <chuotyeugaox9@gmail.com>
2017-06-23 15:48:44 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package bignet
+ */
+
+?>
+
+<!-- #content -->
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<nav class="navbar navbar-default container-fluid navbar-inverse" role="navigation">
+			<div class="container-fluid">
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+					<h3 class="text">CỬA HÀNG CHÚNG TÔI </h3><hr>
+					<h5 class="small">Giới thiệu về cửa hàng</h5><hr>
+					<h5 class="small">Liên hệ với chúng tôi</h5><hr>
+					
+				</div>
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+					<h3 class="text">HÃY ĐẾN VỚI CHÚNG TÔI</h3><hr>
+					<h5 class="small">website</h5><hr>
+					<h5 class="small">email</h5><hr>
+				</div>
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+				   <h3 class="text" >THEO DÕI CHÚNG TÔI </h3><hr>	    
+				    <h5 class="small">facebook</h5><hr>	               				
+					<h5 class="small">email</h5><hr>				
+	             
+				</div>	
+			</div>
+		</nav>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+<?php wp_footer(); ?>
+
+</body>
+</html>
