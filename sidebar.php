b1d64234b87c78e68274ad43cd12e50278962541
minhtu250695 <chuotyeugaox9@gmail.com>
2017-06-23 15:47:40 +0700

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package bignet
+ */
+
+
+
+
+if ( ! is_active_sidebar( 'sidebar' ) ) {
+	return;
+
+}
+
+?>
+				
+<?php dynamic_sidebar( 'sidebar' ); ?>
+
+<div class="box_video">
+   <div class="box_right_group hidden-xs hidden-sm">
+		<div class="title_box_right_group">BẢNG GIÁ GẠO </div>
+			<div class="inner_right_group">
+				<div id=" frame_video" >
+				 	<div class="row">
+						<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+							<p>Loại gạo</p>
+							<hr>
+							<p>Trân Châu</p>
+							<hr>
+							<p>Bắc Hương</p>
+							<hr>
+							<p>Lài Sữa</p>
+							<hr>
+							<p>Nàng Xuân</p>
+							<hr>
+							<p>Nếp Cái HV</p>
+							<hr>
+							<p>ST đỏ</p>
+							<hr>
+							<p>Tám Hải Hậu</p>
+							<hr>
+							<p>Tám Đài Loan</p>
+							<hr>
+							<p>Tám Điện Biên</p>
+							<hr>
+							<p>Thơm Thái</p>
+							<hr>
+							<p>Gạo Tẻ Ngon</p>
+							<hr>
+							<p>Gạo Tẻ </p>
+							 <hr>	  		
+						</div>
+						<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+							  <p>Giá / Kg</p>
+							  <hr>		
+							  <p class="gia">24 200</p>
+							  <hr>	
+							  <p class="gia">15 200</p>
+							  <hr>	
+							  <p class="gia">19 800</p>
+							  <hr>	
+							  <p class="gia">22 000</p>
+							  <hr>	
+							  <p class="gia">24 000</p>
+							  <hr>	
+							  <p class="gia">24 000</p>
+							  <hr>	
+							   <p class="gia">24 000</p>
+							  <hr>		
+							  <p class="gia">17 800</p>
+							  <hr>	
+							  <p class="gia">18 600</p>
+							  <hr>	
+							  <p class="gia">13 800</p>
+							  <hr>	
+							  <p class="gia">13 200</p>
+							  <hr>	
+							  <p class="gia">9 000</p>
+							  <hr> 
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="luuy">
+				<p class="mau">Lưu ý : bảng giá trên làm giá tham khảo Quý vị mua gạo xin hãy gọi điện trưc tiếp các điểm bán gạo để được cung cấp báo giá mới nhất.</p>
+		   </div>
+	</div>
+</div>
